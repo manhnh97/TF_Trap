9817c82de595d93d8b1f9837720088bc51467c69
manhnh97 <manhnh97@gmail.com>
2024-04-27 13:59:55 +0700

    Updatedgit rm -rf  .!

--- QuanX-dev/Scripts/jike/jike.live.js
+++
@@ -1,8 +0,0 @@
-let body = JSON.parse($response.body);
-
-if (/^https:\/\/api\.ruguoapp\.com\/1\.0\/recommendFeed\/list/.test($request.url)) {
-  body.data = body.data.filter((d) => !d.live && !d.lives);
-  body = JSON.stringify(body);
-}
-
-$done({ body });
--- QuanX-dev/Scripts/weibo/weibo.hot.js
+++
@@ -1,6 +0,0 @@
-let body = JSON.parse($response.body);
-
-body.cards[0].card_group = body.cards[0].card_group.filter(c=>!c.itemid.includes("t:51"));
-body = JSON.stringify(body);
-
-$done({ body });
