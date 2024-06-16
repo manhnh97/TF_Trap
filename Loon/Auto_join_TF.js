8d96e10c0256366c97a630163e1d55a9145ea375
ManhNH97 <manhnh97@gmail.com>
2024-06-16 22:29:18 +0700

    Update new code

--- Loon/Auto_join_TF.js
+++ Loon/Auto_join_TF.js
@@ -5,68 +5,88 @@
 感谢某位大佬将改写为Loon版脚本！
 */
 !(async () => {
-  ids = $persistentStore.read('APP_ID')
+  ids = $persistentStore.read("APP_ID");
   if (ids == null) {
-    $notification.post('APP_ID đang trống', 'Thêm APP_ID ngay', '')
-  } else if (ids == '') {
-    $notification.post('Đã thêm đủ, kiểm tra danh sách TestFlight app', 'Tắt Plugin và thêm các ứng dụng khác', '')
+    $notification.post(
+      "未添加 TestFlight APP_ID",
+      "请手动添加或使用 TestFlight 链接自动获取",
+      ""
+    );
+  } else if (ids == "") {
+    $notification.post("所有 TestFlight 已加入完毕", "请手动禁用该插件", "");
   } else {
-    ids = ids.split(',')
+    ids = ids.split(",");
     for await (const ID of ids) {
-      await autoPost(ID)
+      await autoPost(ID);
     }
   }
-  $done()
-})()
+  $done();
+})();
 
 function autoPost(ID) {
-  let Key = $persistentStore.read('key')
-  let testurl = 'https://testflight.apple.com/v3/accounts/' + Key + '/ru/'
+  let Key = $persistentStore.read("key");
+  let testurl = "https://testflight.apple.com/v3/accounts/" + Key + "/ru/";
   let header = {
-    'X-Session-Id': `${$persistentStore.read('session_id')}`,
-    'X-Session-Digest': `${$persistentStore.read('session_digest')}`,
-    'X-Request-Id': `${$persistentStore.read('request_id')}`,
-    'User-Agent': `${$persistentStore.read('tf_ua')}`,
-  }
+    "X-Session-Id": `${$persistentStore.read("session_id")}`,
+    "X-Session-Digest": `${$persistentStore.read("session_digest")}`,
+    "X-Request-Id": `${$persistentStore.read("request_id")}`,
+    "User-Agent": `${$persistentStore.read("tf_ua")}`,
+  };
   return new Promise(function (resolve) {
-    $httpClient.get({ url: testurl + ID, headers: header }, function (error, resp, data) {
-      if (error == null) {
-        if (resp.status == 404) {
-          ids = $persistentStore.read('APP_ID').split(',')
-          ids = ids.filter(ids => ids !== ID)
-          $persistentStore.write(ids.toString(), 'APP_ID')
-          console.log(ID + ' ' + 'TestFlight không tồn tại，APP_ID đã tự động xóa')
-          $notification.post(ID + ' ' + 'TestFlight không tồn tại，APP_ID đã tự động xóa')
-          resolve()
-        } else {
-          let jsonData = JSON.parse(data)
-          if (jsonData.data == null) {
-            console.log(ID + ' ' + jsonData.messages[0].message)
-            resolve()
-          } else if (jsonData.data.status == 'FULL') {
-            console.log(jsonData.data.app.name + ' ' + ID + ' ' + jsonData.data.message)
-            resolve()
+    $httpClient.get(
+      { url: testurl + ID, headers: header },
+      function (error, resp, data) {
+        if (error == null) {
+          if (resp.status == 404) {
+            ids = $persistentStore.read("APP_ID").split(",");
+            ids = ids.filter((ids) => ids !== ID);
+            $persistentStore.write(ids.toString(), "APP_ID");
+            console.log(ID + " " + "不存在该 TestFlight，已自动删除该 APP_ID");
+            $notification.post(
+              ID,
+              "不存在该 TestFlight",
+              "已自动删除该 APP_ID"
+            );
+            resolve();
           } else {
-            $httpClient.post({ url: testurl + ID + '/accept', headers: header }, function (error, resp, body) {
-              let jsonBody = JSON.parse(body)
-              $notification.post(jsonBody.data.name, 'TestFlight đã tham gia thành công', '')
-              console.log(jsonBody.data.name + ' ' + 'TestFlight đã tham gia thành công')
-              ids = $persistentStore.read('APP_ID').split(',')
-              ids = ids.filter(ids => ids !== ID)
-              $persistentStore.write(ids.toString(), 'APP_ID')
-              resolve()
-            })
+            let jsonData = JSON.parse(data);
+            if (jsonData.data == null) {
+              console.log(ID + " " + jsonData.messages[0].message);
+              resolve();
+            } else if (jsonData.data.status == "FULL") {
+              console.log(
+                jsonData.data.app.name + " " + ID + " " + jsonData.data.message
+              );
+              resolve();
+            } else {
+              $httpClient.post(
+                { url: testurl + ID + "/accept", headers: header },
+                function (error, resp, body) {
+                  let jsonBody = JSON.parse(body);
+                  $notification.post(
+                    jsonBody.data.name,
+                    "TestFlight 加入成功",
+                    ""
+                  );
+                  console.log(jsonBody.data.name + " TestFlight 加入成功");
+                  ids = $persistentStore.read("APP_ID").split(",");
+                  ids = ids.filter((ids) => ids !== ID);
+                  $persistentStore.write(ids.toString(), "APP_ID");
+                  resolve();
+                }
+              );
+            }
+          }
+        } else {
+          if (error == "The request timed out.") {
+            resolve();
+          } else {
+            $notification.post("自动加入 TestFlight", error, "");
+            console.log(ID + " " + error);
+            resolve();
           }
         }
-      } else {
-        if (error == 'The request timed out.') {
-          resolve()
-        } else {
-          $notification.post('TestFlight có lỗi: ', error, '')
-          console.log(ID + ' ' + error)
-          resolve()
-        }
       }
-    })
-  })
+    );
+  });
 }
--- Loon/TF_keys.js
+++ Loon/TF_keys.js
@@ -11,26 +11,33 @@
 const reg1 = /^https:\/\/testflight\.apple\.com\/v3\/accounts\/(.*)\/apps$/;
 const reg2 = /^https:\/\/testflight\.apple\.com\/join\/(.*)/;
 if (reg1.test($request.url)) {
-    $persistentStore.write(null, 'request_id')
-    let url = $request.url
-    let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, '$2')
-    let session_id = $request.headers['X-Session-Id'] || $request.headers['x-session-id']
-    let session_digest = $request.headers['X-Session-Digest'] || $request.headers['x-session-digest']
-    let request_id = $request.headers['X-Request-Id'] || $request.headers['x-request-id']
-    let ua = $request.headers['User-Agent'] || $request.headers['user-agent']
-    $persistentStore.write(key, 'key')
-    $persistentStore.write(session_id, 'session_id')
-    $persistentStore.write(session_digest, 'session_digest')
-    $persistentStore.write(request_id, 'request_id')
-    $persistentStore.write(ua, 'tf_ua')
-    console.log($request.headers)
-    if ($persistentStore.read('request_id') !== null) {
-      $notification.post('Lấy thông tin TF', 'Lấy thành công, vui lòng đóng kịch bản','')
-
-    } else {
-      $notification.post('Lấy thông tin TF','Không lấy được thông tin, bật Mitm => HTTP2, khởi động lại VPN và TestFlight App','')
-    }
-    $done({})
+  $persistentStore.write(null, "request_id");
+  let url = $request.url;
+  let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, "$2");
+  let session_id =
+    $request.headers["X-Session-Id"] || $request.headers["x-session-id"];
+  let session_digest =
+    $request.headers["X-Session-Digest"] ||
+    $request.headers["x-session-digest"];
+  let request_id =
+    $request.headers["X-Request-Id"] || $request.headers["x-request-id"];
+  let ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+  $persistentStore.write(key, "key");
+  $persistentStore.write(session_id, "session_id");
+  $persistentStore.write(session_digest, "session_digest");
+  $persistentStore.write(request_id, "request_id");
+  $persistentStore.write(ua, "tf_ua");
+  console.log($request.headers);
+  if ($persistentStore.read("request_id") !== null) {
+    $notification.post("TF信息获取", "信息获取成功，请关闭脚本！", "");
+  } else {
+    $notification.post(
+      "TF信息获取",
+      "信息获取失败，请打开Mitm over HTTP2开关，并重启VPN和TestFlight App！",
+      ""
+    );
+  }
+  $done({});
 }
 if (reg2.test($request.url)) {
   let appId = $persistentStore.read("APP_ID");
@@ -45,8 +52,12 @@ if (reg2.test($request.url)) {
     appId = arr.join(",");
   }
   $persistentStore.write(appId, "APP_ID");
-  $notification.post("Tự động tham gia TestFlight", `Đã thêm APP_ID: ${id}`, `Còn lại ID(s): ${appId}`);
-  $done({})
+  $notification.post(
+    "TestFlight自动加入",
+    `已添加APP_ID: ${id}`,
+    `当前ID: ${appId}`
+  );
+  $done({});
 }
 function unique(arr) {
   return Array.from(new Set(arr));
